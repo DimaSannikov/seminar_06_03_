4f63f0e2e5ed210da68a1953f6f3670b5ce77eb8
DimaSannikov <dimasannikov@gmail.com>
2023-03-10 01:42:28 +0500

    added EX_03

---
+++ EX_03/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задайте массив. Напишите программу, которая определяет,
+// присутствует ли заданное число в массиве.
+
+int[] MakeArray(int len)
+{
+    int[] array = new int[len];
+    for(int i = 0; i < array.Length; i++)
+    {
+        int num = new Random().Next(-9, 10);
+        array[i] = num;
+    }
+    return array;
+}
+
+
+void SearchElem(int num, int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(num == array[i])
+        {
+            Console.WriteLine($"Элемент {num} есть в массиве");
+        }
+    }
+}
+
+
+int[] array = MakeArray(12);
+SearchElem(2, array);
