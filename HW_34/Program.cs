35b4d45dd2877cd5218d58f93d009c2df3830b66
DimaSannikov <dimasannikov@gmail.com>
2023-03-10 01:42:58 +0500

    added HW_34

---
+++ HW_34/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int[] MakeArray(int len)
+{
+    int[] array = new int[len];
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100, 999);
+    }
+    return array;
+}
+
+
+int SearchEvenNumbers(int[] array)
+{
+    int count = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i] % 2 == 0) count++;
+    }
+    return count;
+}
+
+
+void PrintArray(int[] array)
+{
+    Console.WriteLine("["+$"{String.Join(", ", array)}"+"]");
+}
+
+
+int[] array = MakeArray(20);
+PrintArray(array);
+
+int count = SearchEvenNumbers(array);
+Console.WriteLine(count);
