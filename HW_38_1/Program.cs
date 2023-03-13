209152f859a855ee43ee952ba4993ec7ddbc6cbb
DimaSannikov <dimasannikov@gmail.com>
2023-03-13 22:28:50 +0500

    added HW_38_1

---
+++ HW_38_1/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между
+// максимальным и минимальным элементов массива.
+
+double[] MakeArray(int len)
+{
+    double[] array = new double[len];
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = Math.Round(new Random().NextDouble() * 100, 3);
+    }
+    return array;
+}
+
+
+double FindDifference(double[] array)
+{
+    double maxElem = array[0];
+    double minElem = array[0];
+    for(int i = 1; i < array.Length; i++)
+    {
+        if(array[i] > maxElem) maxElem = array[i];
+        if(array[i] < minElem) minElem = array[i];
+    }
+    double diff = maxElem - minElem;
+    return diff;
+}
+
+
+void PrintArray(double[] array)
+{
+    Console.WriteLine("["+$"{String.Join(", ", array)}"+"]");
+}
+
+
+double[] array = MakeArray(10);
+PrintArray(array);
+
+double difference = FindDifference(array);
+Console.WriteLine(difference);
